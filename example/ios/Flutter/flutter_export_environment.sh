377a688e38af51bfe158f05b41e4cee7659eb6b2
kanari3 <kanari333333@gmail.com>
2021-03-24 11:10:41 +0900

    update lock files

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/daisuke/Develop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/daisuke/Develop/flutter_calendar_carousel/example"
-export "FLUTTER_TARGET=/Users/daisuke/Develop/flutter_calendar_carousel/example/lib/main.dart"
+export "FLUTTER_ROOT=/Users/admin/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/admin/work/andpad/flutter_calendar_carousel/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/daisuke/Develop/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
